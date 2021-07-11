c20ee3e35468f5b38182a9dbc3545d1ca2b1e2ff
tanvi-488 <73576515+tanvi-488@users.noreply.github.com>
2021-07-11 18:20:59 +0530

    Add files via upload

---
+++ server/app/socketManager.js
@@ -0,0 +1,12 @@
+const io = require("../index").io;
+
+module.exports = (socket) => {
+  try {
+    console.log("Connected");
+    socket.on("code", (data, callback) => {
+      socket.broadcast.emit("code", data);
+    });
+  } catch (ex) {
+    console.log(ex.message);
+  }
+};
